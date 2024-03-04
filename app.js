6c3e9e0640c5281614c338f2f263ef9eb46de1e1
sadia saadi <saadisadia204@gmail.com>
2024-03-04 13:14:27 +0500

    assignment no 16

---
+++ app.js
@@ -0,0 +1,107 @@
+// chap :USER INPUT AND CONDITIONAL SATAEMENT
+
+// q;1
+// var city=prompt("enter your city name");
+// if (city==="karachi"){alert("welcome to city of light")};
+
+// q2
+// var gender=prompt("enter your gender");
+// if (gender==="female"){alert("GOOD MORNING MAM")};
+
+// q;3
+// var gender=prompt("enter your gender");
+// if (gender==="female"){alert("GOOD MORNING MAM")};
+
+// q;5
+//  var a = 4;
+//  if (++a === 5){
+//  alert("given condition for variable a is true");
+//  }
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// e. if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+// f. if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+// Q;8
+// var number=prompt("enter a number");
+// if(number%3){
+//     console.log(number+"is divisible by 3")
+// };
+// else{console.log(
+//     number+"is not divisible by 3"
+// )};
+
+
+
+// chap 10-14
+// q;1
+// var city =prompt("plz enter a city")
+// if (city === "Karachi") {
+//     console.log("The City OF Lights");
+// }
+
+// q;2
+// if (x === y) {
+//     let z = prompt("Enter the value of z:");
+// console.log(z+"20")
+// }
+
+// q;3
+// var ipCode=prompt("enter a code")
+// if(ipCode==="10010"){
+//     alert("karachi");
+// }
+// else{alert("plz enter a right code")};
+
+// q;4
+// let x = 1;
+
+// if (x === 1) {
+//     x = 2; 
+// }
+
+// console.log("x"); 
+
+
+// cah if and else statement
+// q;1
+var varaible=prompt("enter a number")
+var a=3
+var b=5
+if(a<=b){
+    alert("varaible b is greater an equal to a")
+}
+else{
+    alert("varaible a is less tan to b")
+};
+
+
